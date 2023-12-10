5b38f98cae5c1f1fad99fe51e008297b574b2440
Siddharth Bhaskar <sidbhaskar1111@gmail.com>
2023-12-10 15:46:46 +0530

    Commit

---
+++ Python Class Files/Some more code/listttt.py
@@ -0,0 +1,3 @@
+l = [12,34,5,5,67,7,8,8]
+print(l.count(2))
+print(l.index(7,1,7))
---
+++ Python Class Files/Some more code/vote.py
@@ -0,0 +1,5 @@
+from datetime import datetime , timedelta
+
+now = datetime.now()
+DOB = int(input('Enter your DOB in pattern (yyyy-mm-dd'))
+birthday = datetime.strptime(DOB, "%Y-")
