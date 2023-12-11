5901e11c79a605788f47977155c05242e0d73e5f
Siddharth Bhaskar <sidbhaskar1111@gmail.com>
2023-12-11 19:51:31 +0530

    Commit

---
+++ Python Class Files/Some more code/atm.py
@@ -0,0 +1,7 @@
+note_100 = int(input("100: "))
+note_500 = int(input("500: "))
+note_1000 = int(input("1000: "))
+
+total = note_100*100 + note_500*500 + note_1000*1000
+
+print("total cash" , total)
