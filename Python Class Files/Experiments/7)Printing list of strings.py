169f1bac93656b26e6d9214768d1414742dfdece
Siddharth Bhaskar <sidbhaskar1111@gmail.com>
2023-12-13 20:34:29 +0530

    Commit

--- Python Class Files/Experiments/7)Printing list of strings.py
+++ Python Class Files/Experiments/7)Printing list of strings.py
@@ -1,3 +1,4 @@
 sentence = input('Enter your sentence : ')
 words = sentence.split()
 print('List of words are : ',words)
+
--- Python Class Files/Some more code/hello.py
+++ Python Class Files/Some more code/hello.py
@@ -1,1 +1,4 @@
-print('hello world')
+list = [1,1,2,2,3,4,5,5]
+st = set(list)
+list2 = list(st)
+print(list2)
