b6001e41dff23005761648ad74d9d935f908fdb7
Siddharth Bhaskar <sidbhaskar1111@gmail.com>
2023-12-28 09:54:46 +0530

    Commit

---
+++ Python Class Files/Module 5/gui/combobox.py
@@ -0,0 +1,14 @@
+from tkinter import *
+from tkinter import ttk
+
+top = Tk()
+top.geometry("200x200")
+
+frame = Frame(top)
+frame.pack()
+
+langs = ['C','C++','Java','Python']
+Combo = ttk.Combobox(frame, values=langs)
+Combo.set('Choose your language')
+Combo.pack(padx=5,pady=5)
+top.mainloop()
---
+++ Python Class Files/Module 5/gui/progress bar.py
@@ -0,0 +1,25 @@
+import tkinter as tk
+import tkinter.ttk as ttk
+from tkinter import simpledialog
+
+root = tk.Tk()
+
+data = [
+    ['Bobby',26,20000],['Harrish',20,20700],['Jaya',18,19000],['Mark',27,25000],
+]
+
+index=0
+def read_data():
+    for index, line in enumerate(data):
+        tree.insert('',tk.END,iid=index, text=line[0], values=line [1:])
+columns = ('age','salary')
+
+tree = ttk.Treeview(root, columns= columns, height =20)
+tree.pack(padx=5,pady=5)
+
+tree.heading('#0',text='Name')
+tree.heading('age',text='Age')
+tree.heading('salary',text='Salary')
+
+read_data()
+root.mainloop()
