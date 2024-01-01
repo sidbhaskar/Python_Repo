13c5194a70f2a95f7b85c535aef617b49a8db222
Siddharth Bhaskar <sidbhaskar1111@gmail.com>
2024-01-01 13:52:13 +0530

    Commit

--- Python Class Files/Module 5/gui/random gui.py
+++ Python Class Files/Module 5/gui/random gui.py
@@ -1,1 +1,13 @@
-print('Helllo World')
+from tkinter import *
+from tkinter.colorchooser import askcolor
+
+m = Tk()
+m.geometry('200x100')
+
+def show():
+    color = askcolor()
+    print(color)
+
+B = Button(m,text='Click Me', command=show)
+B.place(x=50,y=50)
+m.mainloop()
